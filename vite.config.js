781ae53d9480bc37b147ac40be884b9c517a9bba
YatoCrow <yatocrowgames@gmail.com>
2025-05-19 23:47:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/', // 👈 add this line
   plugins: [
     react(),
     VitePWA({
